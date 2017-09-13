1e01c17a3d0a30a73ea96ec133090896482053c2
twist525 <twist7393@gmail.com>
2017-09-13 13:51:59 +0500

    39

--- images/js/UniCode.js
+++
@@ -1,7 +0,0 @@
-﻿// Создаем переменную для хранения некоторого числа в диапазоне от 10000 до 99999
-var randomNum = Math.floor((Math.random() * 99999) + 10000);
-
-// Создаем переменную el для записи элемента с идентификатором info
-var el = document.getElementById('UniCode');
-// Записываем число в элемент
-el.innerHTML = '<p>Jm' + randomNum + '</p>';
